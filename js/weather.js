5b3e6ad0b58a3c551c8b51eee00b843cd13d74c8
LTDKRRRR <a12345dkr@163.com>
2023-04-15 22:33:46 +0800

    Site updated: 2023-04-15 22:33:39

---
+++ js/weather.js
@@ -0,0 +1,21 @@
+WIDGET = {
+    "CONFIG": {
+      "modules": "01234",
+      "background": "1",
+      "tmpColor": "FFFFFF",
+      "tmpSize": "16",
+      "cityColor": "FFFFFF",
+      "citySize": "16",
+      "aqiColor": "FFFFFF",
+      "aqiSize": "16",
+      "weatherIconSize": "24",
+      "alertIconSize": "18",
+      "padding": "10px 10px 10px 10px",
+      "shadow": "0",
+      "language": "auto",
+      "fixed": "false",
+      "vertical": "top",
+      "horizontal": "left",
+      "key": "0d50ba7e75184679a5fc9683c024eef5"
+    }
+  }
